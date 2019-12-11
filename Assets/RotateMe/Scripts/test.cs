0d90a3e896d0f618819d4074fc1008cbf84f12e8
zhoumingliang <407044451@qq.com>
2019-12-11 12:05:36 +0800

    add test

---
+++ Assets/RotateMe/Scripts/test.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Coffee.RotateMe
+{
+	public class test : MonoBehaviour
+	{
+		public float angularVelocity = 10;
+		void Update ()
+		{
+			
+		}
+	}
+}
---
+++ Assets/RotateMe/Scripts/test1.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Coffee.RotateMe
+{
+    public class test1 : MonoBehaviour
+    {
+        public float angularVelocity = 10;
+        void Update ()
+        {
+			
+        }
+    }
+}
