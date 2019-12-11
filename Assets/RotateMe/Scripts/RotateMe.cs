d43ef5c000561e77cf2cbf59d8488acd61325298
zhoumingliang <407044451@qq.com>
2019-12-11 11:38:22 +0800

    add .

---
+++ Assets/RotateMe/Scripts/RotateMe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Coffee.RotateMe
+{
+	public class RotateMe : MonoBehaviour
+	{
+		public float angularVelocity = 10;
+		void Update ()
+		{
+			var rot = Quaternion.AngleAxis (angularVelocity * Time.deltaTime, Vector3.up);
+			transform.localRotation = rot * transform.localRotation;
+			Debug.Log("hogehogehoge");
+			Debug.Log("fugafugas");
+			Debug.Log("fix bugs1");
+		}
+	}
+}
